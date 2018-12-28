804180bcce19399be8db74535aa0853cd1c547ef
Thomas Brun <thomas@Thomass-MacBook-Air.local>
2018-12-28 14:41:14 +0100

    Initial Commit

---
+++ SwissMeme FanApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwissMeme FanApp
+//
+//  Created by Thomas Brun on 28.12.18.
+//  Copyright © 2018 Thomas Brun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
